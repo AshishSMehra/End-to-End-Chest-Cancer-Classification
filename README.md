5d8de97785de0834c62f93085848a6d58afb8749
Ashish Mehra <ashishmehra1007@gmail.com>
2025-04-12 16:10:15 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # End-to-End-Chest-Cancer-Classification
+
+
+
+
+## For Dagshub 
+
+import dagshub
+dagshub.init(repo_owner='AshishSMehra', repo_name='End-to-End-Chest-Cancer-Classification', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
+
+
+  
---
+++ template.py
@@ -0,0 +1,47 @@
+import os
+from pathlib import Path
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+# Project name
+project_name = "cnnClassifier"
+
+# List of files and folders to be created
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+]
+
+# Create files and directories
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir = filepath.parent
+    filename = filepath.name
+
+    # Create directories if they don't exist
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+
+    # Create file if it doesn't exist or is empty
+    if not filepath.exists() or filepath.stat().st_size == 0:
+        filepath.touch()
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"File already exists: {filepath}")
